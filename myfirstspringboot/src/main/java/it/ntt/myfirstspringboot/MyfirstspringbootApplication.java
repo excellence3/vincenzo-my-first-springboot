29fa939708eb48a7e600f68a01addd8a438df5ed
VincenzoD92 <vincenzo.denisi@nttdata.com>
2021-12-10 15:13:58 +0100

    first commit

---
+++ myfirstspringboot/src/main/java/it/ntt/myfirstspringboot/MyfirstspringbootApplication.java
@@ -0,0 +1,13 @@
+package it.ntt.myfirstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringbootApplication.class, args);
+	}
+
+}
